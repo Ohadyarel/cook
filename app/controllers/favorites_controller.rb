bc14d511c8975bfb9ccdbf8b93ba44e7c2420202
Ohad Yarel <ohadyarel@gmail.com>
2016-06-17 17:06:19 -0400

    create favorite/unfavorite feature on show#recipe

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,26 @@
 class FavoritesController < ApplicationController
 	def create
-		@favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:id] )	
-		respond_to do |format|
-			if @favorite.save
-				format.js
-			else
-				format.html { redirect_to :back }
-			end
+		@favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:recipe_id])	
+		if @favorite.save
+			redirect_to :back
 		end
+		# respond_to do |format|
+		# 	if @favorite.save
+		# 		format.js
+		# 	else
+		# 		format.html { redirect_to :back }
+		# 	end
+		# end
 	end
 
 	def destroy
 		@favorite = Favorite.find(params[:id])
-		@favorite.destroy
 		respond_to do |format|
-			format.js
-		else
-			format.html { redirect_to :back }
-		end 
+			if @favorite.destroy
+				format.js
+			else
+				format.html { redirect_to :back }
+			end 
+		end
 	end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,6 @@ class RecipesController < ApplicationController
 
 	def show
 		@recipe = Recipe.find(params[:id])
+		@favorite = Favorite.where(recipe_id: params[:id], user_id: current_user.id).first if logged_in?
 	end
 end
